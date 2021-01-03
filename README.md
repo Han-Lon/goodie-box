02d7b91199641071a97145728b794b7206bb6c45
root <root@LAPTOP-I12GRTBI.localdomain>
2021-01-02 22:36:17 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # goodie-box
 An Arduino-based solution to preventing multiple pets from eating out of each other's food bowls
+
+## Requirements
+- Uses Miguel Balboa's RFID library located at https://github.com/miguelbalboa/rfid
+  - You will need to download and import this library for this to function
+
+## Roadmap
+- Rewrite using ID12LA RFID reader instead of MFRC522
+- Design 3D printable mounts for RFID reader and Arduino components
+- Build the dang box
---
+++ main.ino
@@ -0,0 +1,61 @@
+/*
+ * Utilizes code from https://create.arduino.cc/projecthub/Aritro/security-access-using-rfid-reader-f7c746
+ * Then modified by Hogman the Intruder
+ * 
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Waiting for RFID devices...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  // convert bytes to human-readable characters
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "1A BB C2 82") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    tone(8, 4000, 1000); // trigger the buzzer on failed access
+  }
+} 
